adf117fa665a0bc1d4d1ab1108abc1e6c444fc0f
Kevin <stradtkt22@gmail.com>
2017-10-17 11:43:27 -0400

    Change the navigation on the buy-theme-page, so it only has home on the navbar

--- buy-now.php
+++ buy-now.php
@@ -1,5 +1,18 @@
 <?php include('assets/inc/head.php'); ?>
-<?php include('assets/inc/nav.php'); ?>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+  <a class="navbar-brand" href="#">Company Name</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 
 
